c14de94e193347d2d917e19a8285a150843697ae
Jonathon Ringeisen <ringeisen85@gmail.com>
2020-04-03 12:04:59 -0600

    Started adding directory content and designing the layout.

--- public/js/app.js
+++ public/js/app.js
@@ -1899,6 +1899,73 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/DirectoryContent.vue?vue&type=script&lang=js&":
+/*!***************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/DirectoryContent.vue?vue&type=script&lang=js& ***!
+  \***************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      formData: {},
+      cardHovered: false
+    };
+  },
+  methods: {
+    handleHover: function handleHover() {
+      this.cardHovered = !this.cardHovered;
+    },
+    getSize: function getSize() {
+      if (this.cardHovered) {
+        return '6rem';
+      }
+
+      return '5rem';
+    },
+    getBorder: function getBorder() {
+      if (this.cardHovered) {
+        return 'secondary';
+      }
+
+      return 'light';
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/HomeContent.vue?vue&type=script&lang=js&":
 /*!**********************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/HomeContent.vue?vue&type=script&lang=js& ***!
@@ -62691,6 +62758,122 @@ var e=function(){return(e=Object.assign||function(e){for(var t,r=1,s=arguments.l
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/DirectoryContent.vue?vue&type=template&id=7ab115b7&":
+/*!*******************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/DirectoryContent.vue?vue&type=template&id=7ab115b7& ***!
+  \*******************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "pt-5" }, [
+    _c("div", { staticClass: "row justify-content-center" }, [
+      _c(
+        "div",
+        { staticClass: "col-md-10 pb-3" },
+        [
+          _c("b-form-input", {
+            attrs: {
+              "v-model": _vm.formData,
+              placeholder: "Search for photographer....."
+            }
+          })
+        ],
+        1
+      )
+    ]),
+    _vm._v(" "),
+    _c("div", { staticClass: "row justify-content-center pb-3" }, [
+      _c(
+        "div",
+        { staticClass: "col-md-10" },
+        [
+          _c(
+            "b-card",
+            {
+              directives: [
+                {
+                  name: "b-hover",
+                  rawName: "v-b-hover",
+                  value: _vm.handleHover,
+                  expression: "handleHover"
+                }
+              ],
+              staticClass: "overflow-hidden shadow",
+              staticStyle: { cursor: "pointer" },
+              attrs: { "no-body": "", "border-variant": _vm.getBorder() }
+            },
+            [
+              _c(
+                "b-row",
+                {
+                  staticClass: "d-flex align-items-center",
+                  attrs: { "no-gutters": "" }
+                },
+                [
+                  _c(
+                    "b-col",
+                    {
+                      staticClass: "d-flex justify-content-center",
+                      attrs: { md: "2" }
+                    },
+                    [
+                      _c("b-avatar", {
+                        attrs: {
+                          variant: "secondary",
+                          src: "https://placekitten.com/300/300",
+                          size: _vm.getSize()
+                        }
+                      })
+                    ],
+                    1
+                  ),
+                  _vm._v(" "),
+                  _c(
+                    "b-col",
+                    { attrs: { md: "10" } },
+                    [
+                      _c(
+                        "b-card-body",
+                        { attrs: { title: "Jonathon Ringeisen" } },
+                        [
+                          _c("b-card-text", { attrs: { size: "10rem" } }, [
+                            _vm._v(
+                              "\n                                I specialize in Rafting and Kayaking Photos on the Alberton Gorge Section of the Clark Fork River. Hopefully I will see you at the Fang Rapid! Please contact me if you have any questions.\n                            "
+                            )
+                          ])
+                        ],
+                        1
+                      )
+                    ],
+                    1
+                  )
+                ],
+                1
+              )
+            ],
+            1
+          )
+        ],
+        1
+      )
+    ])
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/HomeContent.vue?vue&type=template&id=3e008c7f&":
 /*!**************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/HomeContent.vue?vue&type=template&id=3e008c7f& ***!
@@ -75032,6 +75215,7 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 var map = {
+	"./components/DirectoryContent.vue": "./resources/js/components/DirectoryContent.vue",
 	"./components/HomeContent.vue": "./resources/js/components/HomeContent.vue",
 	"./partials/TopNavigation.vue": "./resources/js/partials/TopNavigation.vue"
 };
@@ -75124,6 +75308,75 @@ var app = new Vue({
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/***/ }),
+
+/***/ "./resources/js/components/DirectoryContent.vue":
+/*!******************************************************!*\
+  !*** ./resources/js/components/DirectoryContent.vue ***!
+  \******************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _DirectoryContent_vue_vue_type_template_id_7ab115b7___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./DirectoryContent.vue?vue&type=template&id=7ab115b7& */ "./resources/js/components/DirectoryContent.vue?vue&type=template&id=7ab115b7&");
+/* harmony import */ var _DirectoryContent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./DirectoryContent.vue?vue&type=script&lang=js& */ "./resources/js/components/DirectoryContent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _DirectoryContent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _DirectoryContent_vue_vue_type_template_id_7ab115b7___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _DirectoryContent_vue_vue_type_template_id_7ab115b7___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/DirectoryContent.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/DirectoryContent.vue?vue&type=script&lang=js&":
+/*!*******************************************************************************!*\
+  !*** ./resources/js/components/DirectoryContent.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_DirectoryContent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./DirectoryContent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/DirectoryContent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_DirectoryContent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/DirectoryContent.vue?vue&type=template&id=7ab115b7&":
+/*!*************************************************************************************!*\
+  !*** ./resources/js/components/DirectoryContent.vue?vue&type=template&id=7ab115b7& ***!
+  \*************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_DirectoryContent_vue_vue_type_template_id_7ab115b7___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./DirectoryContent.vue?vue&type=template&id=7ab115b7& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/DirectoryContent.vue?vue&type=template&id=7ab115b7&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_DirectoryContent_vue_vue_type_template_id_7ab115b7___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_DirectoryContent_vue_vue_type_template_id_7ab115b7___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
 
 /***/ }),
 
@@ -75283,8 +75536,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/jonathonringeisen/www/laravel-bootstrapvue-boilerplate/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/jonathonringeisen/www/laravel-bootstrapvue-boilerplate/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/jonathonringeisen/www/esm-directory/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/jonathonringeisen/www/esm-directory/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/js/components/DirectoryContent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="pt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-10 pb-3">
+                <b-form-input :v-model="formData" placeholder="Search for photographer....."></b-form-input>
+            </div>
+        </div>
+        <div class="row justify-content-center pb-3">
+            <div class="col-md-10">
+                <b-card no-body :border-variant="getBorder()" class="overflow-hidden shadow" v-b-hover="handleHover" style="cursor:pointer">
+                    <b-row no-gutters class="d-flex align-items-center">
+                        <b-col md="2" class="d-flex justify-content-center">
+                            <b-avatar variant="secondary" src="https://placekitten.com/300/300" :size="getSize()"></b-avatar>
+                        </b-col>
+                        <b-col md="10">
+                            <b-card-body title="Jonathon Ringeisen">
+                                <b-card-text size="10rem">
+                                    I specialize in Rafting and Kayaking Photos on the Alberton Gorge Section of the Clark Fork River. Hopefully I will see you at the Fang Rapid! Please contact me if you have any questions.
+                                </b-card-text>
+                            </b-card-body>
+                        </b-col>
+                    </b-row>
+                </b-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            formData: {},
+            cardHovered: false
+        }
+    },
+    methods: {
+        handleHover () {
+            this.cardHovered = !this.cardHovered
+        },
+        getSize () {
+            if (this.cardHovered) {
+                return '6rem'
+            }
+            return '5rem'
+        },
+        getBorder () {
+            if (this.cardHovered) {
+                return 'secondary'
+            }
+            return 'light'
+        }
+    }
+}
+</script>
