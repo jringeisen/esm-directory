95737e1297d72f7f2ced8bcd7aab4cc502f3f880
Jonathon Ringeisen <ringeisen85@gmail.com>
2020-04-03 10:50:43 -0600

    First commit.

---
+++ resources/js/components/HomeContent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <b-card header-tag="header">
+                    <template v-slot:header>
+                        <h6 class="mb-0">Example</h6>
+                    </template>
+                    <b-card-text>Example Card with Button</b-card-text>
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/partials/TopNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+    <b-navbar toggleable="sm" type="light" variant="white" class="border">
+        <div class="container">
+            <b-navbar-brand href="/">{{ config }}</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                        <b-nav-item v-if="!name" href="/login">Login</b-nav-item>
+                        <b-nav-item v-if="!name" href="/register">Register</b-nav-item>
+                        
+                        <b-nav-item-dropdown right v-if="name">
+                            <template v-slot:button-content>
+                                {{ name }}
+                            </template>
+                            <b-dropdown-item href="#" @click.prevent="logout">Sign Out</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </div>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            required: true,
+        },
+        config: {
+            require: true,
+        }
+    },
+    methods: {
+        logout () {
+            axios.post('/logout').then(() => {
+                location.reload()
+            })
+        }
+    }
+}
+</script>
