22d455486bc33d5993d9656d021135c22bb9b1fb
Jonathon Ringeisen <ringeisen85@gmail.com>
2020-04-16 14:13:34 -0600

    Imported spatie roles and permissions. Added the HasRoles trait to the user model. Imported the spatie roles and permissions config file.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasApiTokens;
+    use Notifiable, HasApiTokens, HasRoles;
 
     /**
      * The attributes that are mass assignable.
