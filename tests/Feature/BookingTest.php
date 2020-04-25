04a5be97eed3898afabb7fb7f7c0618c5e0a6926
Jonathon Ringeisen <ringeisen85@gmail.com>
2020-04-25 11:04:55 -0600

    Finished the tests that cover the booking controller.

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
+use Carbon\Carbon;
 
+use Tests\TestCase;
 use App\Models\User;
 use App\Models\Booking;
+use App\Mail\BookingConfirmation;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -101,6 +104,137 @@ class BookingTest extends TestCase
             'name' => $data['name'],
             'email' => $data['email'],
             'package' => $data['package']
+        ]);
+    }
+
+    /**
+     * Authenticated user can update a booking.
+     *
+     * @return void
+     */
+    public function testAuthenticatedUserCanUpdateBooking()
+    {
+        $user = $this->authUser();
+        
+        $booking = factory(Booking::class)->create(['user_id' => $user->id]);
+
+        $this->assertDatabaseHas('bookings', [
+            'id' => $booking->id,
+            'user_id' => $booking->user_id,
+            'name' => $booking->name,
+            'email' => $booking->email,
+            'package' => $booking->package
+        ]);
+
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+
+        $this->putJson('/api/bookings/' . $booking->id, $data)
+            ->assertOk()
+            ->assertJson([
+                'name' => $data['name'],
+                'email' => $data['email'],
+            ]);
+
+        $this->assertDatabaseHas('bookings', [
+            'name' => $data['name'],
+            'email' => $data['email']
+        ]);
+    }
+
+    /**
+     * Authenticated user can confirm a booking.
+     *
+     * @return void
+     */
+    public function testAuthenticatedUserCanConfirmABooking()
+    {
+        $user = $this->authUser();
+        
+        $booking = factory(Booking::class)->create(['user_id' => $user->id]);
+
+        $this->assertDatabaseHas('bookings', [
+            'id' => $booking->id,
+            'user_id' => $booking->user_id,
+            'name' => $booking->name,
+            'email' => $booking->email,
+            'package' => $booking->package
+        ]);
+
+        $data = [
+            'confirmed_on' => now()->toDateTimeString()
+        ];
+
+        $this->putJson('/api/bookings/' . $booking->id, $data)
+            ->assertOk();
+
+        $this->assertDatabaseHas('bookings', [
+            'confirmed_on' => Carbon::parse($data['confirmed_on'])->toDateTimeString(),
+        ]);
+    }
+
+    /**
+     * Authenticated user can send confirmation email.
+     *
+     * @return void
+     */
+    public function testAuthenticatedUserCanSentConfirmationEmail()
+    {
+        Mail::fake();
+
+        $user = $this->authUser();
+        
+        $booking = factory(Booking::class)->create(['user_id' => $user->id, 'confirmed_on' => now()]);
+
+        $this->assertDatabaseHas('bookings', [
+            'id' => $booking->id,
+            'user_id' => $booking->user_id,
+            'name' => $booking->name,
+            'email' => $booking->email,
+            'package' => $booking->package,
+            'confirmed_on' => $booking->confirmed_on->toDateTimeString()
+        ]);
+
+        $data = [
+            'send_confirmation' => true
+        ];
+
+        $this->putJson('/api/bookings/' . $booking->id, $data)
+            ->assertOk();
+
+        Mail::assertSent(BookingConfirmation::class, function ($mail) use ($booking) {
+            return $mail->hasTo($booking->email);
+        });
+
+        Mail::assertSent(BookingConfirmation::class, 1);
+    }
+
+    /**
+     * Authenticated user can delete a booking.
+     *
+     * @return void
+     */
+    public function testAuthenticatedUserCanDeleteABooking()
+    {
+        $user = $this->authUser();
+        
+        $booking = factory(Booking::class)->create(['user_id' => $user->id]);
+
+        $this->assertDatabaseHas('bookings', [
+            'user_id' => $user->id,
+            'name' => $booking->name,
+            'email' => $booking->email,
+            'package' => $booking->package
+        ]);
+
+        $this->deleteJson('/api/bookings/' . $booking->id)
+            ->assertOk();
+
+        $this->assertDatabaseMissing('bookings', [
+            'name' => $booking->name,
+            'email' => $booking->email
         ]);
     }
 }
